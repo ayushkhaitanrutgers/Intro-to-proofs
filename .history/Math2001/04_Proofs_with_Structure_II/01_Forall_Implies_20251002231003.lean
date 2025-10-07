b190ae909ca38e45c36970e45e8e04ee89105f47
ayushkhaitanrutgers <157164633+ayushkhaitanrutgers@users.noreply.github.com>
2025-10-06 20:51:22 -0400

    some solutions added

---
+++ .history/Math2001/04_Proofs_with_Structure_II/01_Forall_Implies_20251002231003.lean
@@ -0,0 +1,162 @@
+/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+
+math2001_init
+
+
+example {a : ℝ} (h : ∀ x, a ≤ x ^ 2 - 2 * x) : a ≤ -1 :=
+  calc
+    a ≤ 1 ^ 2 - 2 * 1 := by apply h
+    _ = -1 := by numbers
+
+
+example {n : ℕ} (hn : ∀ m, n ∣ m) : n = 1 := by
+  have h1 : n ∣ 1 := by apply hn
+  have h2 : 0 < 1 := by numbers
+  apply le_antisymm
+  apply Nat.le_of_dvd h2 h1
+  apply Nat.pos_of_dvd_of_pos h1 h2
+
+
+example {a b : ℝ} (h : ∀ x, x ≥ a ∨ x ≤ b) : a ≤ b := by
+  have h1 : (a+b)/2 ≥ a ∨ (a+b)/2 ≤ b := by apply h
+  obtain ha | hb := h1
+  have hc : a/2 ≤ b/2 := by
+    calc
+      a/2 = a - a/2 := by ring
+      _ ≤ (a+b)/2 - a/2 := by rel[ha]
+      _ = a/2 + b/2 - a/2 := by ring
+      _ = b/2 := by ring
+  calc
+    a = a/2 + a/2 := by ring
+    _ ≤ b/2 + b/2 := by rel[hc]
+    _ = b := by ring
+  have hc : a/2 ≤ b/2 := by
+    calc
+      b/2 = b-b/2 := by ring
+      _ ≥ (a+b)/2 - b/2 := by rel[hb]
+      _ = a/2 + b/2 - b/2 := by ring
+      _ = a/2 := by ring
+  calc
+    a = a/2 + a/2 := by ring
+    _ ≤ a/2 + b/2 := by rel[hc]
+    _ = (a+b)/2 := by ring
+    _ ≤ b := by rel[hb]
+
+
+
+
+example {a b : ℝ} (ha1 : a ^ 2 ≤ 2) (hb1 : b ^ 2 ≤ 2) (ha2 : ∀ y, y ^ 2 ≤ 2 → y ≤ a)
+    (hb2 : ∀ y, y ^ 2 ≤ 2 → y ≤ b) :
+    a = b := by
+  apply le_antisymm
+  · apply hb2
+    apply ha1
+  · sorry
+
+example : ∃ b : ℝ, ∀ x : ℝ, b ≤ x ^ 2 - 2 * x := by
+  use -1
+  intro x
+  calc
+    -1 ≤ -1 + (x - 1) ^ 2 := by extra
+    _ = x ^ 2 - 2 * x := by ring
+
+
+example : ∃ c : ℝ, ∀ x y, x ^ 2 + y ^ 2 ≤ 4 → x + y ≥ c := by
+  use -3
+  intro x y h
+  have h1 : (x+y)^2 ≤ 3^2 := by
+    calc
+      (x+y)^2
+        ≤ (x+y)^2 + (x-y)^2 := by extra
+      _ = 2*(x^2+y^2) := by ring
+      _ ≤ 2*4 := by rel[h]
+      _ = 8 := by numbers
+      _ ≤ 9 := by numbers
+      _ = 3^2 := by numbers
+  have hc : 0 ≤ 3 := by numbers
+  have h3 := abs_le_of_sq_le_sq' h1 hc
+  obtain ⟨ ha, hb⟩ := h3
+  exact ha
+
+
+
+example : forall_sufficiently_large n : ℤ, n ^ 3 ≥ 4 * n ^ 2 + 7 := by
+  dsimp
+  use 5
+  intro n hn
+  calc
+    n ^ 3 = n * n ^ 2 := by ring
+    _ ≥ 5 * n ^ 2 := by rel [hn]
+    _ = 4 * n ^ 2 + n ^ 2 := by ring
+    _ ≥ 4 * n ^ 2 + 5 ^ 2 := by rel [hn]
+    _ = 4 * n ^ 2 + 7 + 18 := by ring
+    _ ≥ 4 * n ^ 2 + 7 := by extra
+
+
+example : Prime 2 := by
+  constructor
+  · numbers -- show `2 ≤ 2`
+  intro m hmp
+  have hp : 0 < 2 := by numbers
+  have hmp_le : m ≤ 2 := Nat.le_of_dvd hp hmp
+  have h1m : 1 ≤ m := Nat.pos_of_dvd_of_pos hmp hp
+  interval_cases m
+  · left
+    numbers -- show `1 = 1`
+  · right
+    numbers -- show `2 = 2`
+
+example : Prime 2 := by
+  constructor
+  numbers
+  intro m hm
+  have hp : 0 < 2 := by numbers
+  have h1 : m ≤ 2 := Nat.le_of_dvd hp hm
+  have h2 : m ≥ 1 := Nat.pos_of_dvd_of_pos hm hp
+  interval_cases m
+  left
+  rfl
+  right
+  rfl
+
+example : ¬ Prime 6 := by
+  apply not_prime 2 3
+  · numbers -- show `2 ≠ 1`
+  · numbers -- show `2 ≠ 6`
+  · numbers -- show `6 = 2 * 3`
+
+/-! # Exercises -/
+
+
+example {a : ℚ} (h : ∀ b : ℚ, a ≥ -3 + 4 * b - b ^ 2) : a ≥ 1 := by
+  calc
+    a ≥ -3 + 4*2 - 2^2 := by apply h
+    _ = 1 := by numbers
+
+
+example {n : ℤ} (hn : ∀ m, 1 ≤ m → m ≤ 5 → m ∣ n) : 15 ∣ n := by
+  
+
+example : ∃ n : ℕ, ∀ m : ℕ, n ≤ m := by
+  sorry
+
+example : ∃ a : ℝ, ∀ b : ℝ, ∃ c : ℝ, a + b < c := by
+  sorry
+
+example : forall_sufficiently_large x : ℝ, x ^ 3 + 3 * x ≥ 7 * x ^ 2 + 12 := by
+  use 10
+  intro x hx
+  calc
+    x^3+3*x
+      = x*x^2 + 3*x := by ring
+    _  ≥ 10*x^2+3*10 := by rel[hx]
+    _ = (7*x^2 + 12) + (3*x^2 + 18) := by ring
+    _ ≥ 7*x^2 + 12 := by extra
+
+example : ¬(Prime 45) := by
+  apply not_prime 9 5
+  numbers
+  numbers
+  numbers
---
+++ .history/Math2001/04_Proofs_with_Structure_II/03_Exists_Unique_20251006160610.lean
@@ -0,0 +1,132 @@
+/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+import Library.Theory.ModEq.Defs
+
+math2001_init
+
+namespace Int
+
+
+example : ∃! a : ℝ, 3 * a + 1 = 7 := by
+  use 2
+  dsimp
+  constructor
+  · numbers
+  intro y hy
+  calc
+    y = (3 * y + 1 - 1) / 3 := by ring
+    _ = (7 - 1) / 3 := by rw [hy]
+    _ = 2 := by numbers
+
+
+example : ∃! x : ℚ, ∀ a, a ≥ 1 → a ≤ 3 → (a - x) ^ 2 ≤ 1 := by
+  use 2
+  dsimp
+  constructor
+  intro a ha hb
+  have hc : -1 ≤ (a-2) := by sorry
+  have hd : (a-2) ≤ 1 := by sorry
+  have he: (a-2)^2 ≤ 1^2 := sq_le_sq' hc hd
+  calc
+    (a-2)^2 ≤ 1^2 := by rel[he]
+    _ = 1 := by ring
+
+
+example {x : ℚ} (hx : ∃! a : ℚ, a ^ 2 = x) : x = 0 := by
+  obtain ⟨a, ha1, ha2⟩ := hx
+  have h1 : -a = a
+  · apply ha2
+    calc
+      (-a) ^ 2 = a ^ 2 := by ring
+      _ = x := ha1
+  have h2 :=
+    calc
+      a = (a - -a) / 2 := by ring
+      _ = (a - a) / 2 := by rw [h1]
+      _ = 0 := by ring
+  calc
+    x = a ^ 2 := by rw [ha1]
+    _ = 0 ^ 2 := by rw [h2]
+    _ = 0 := by ring
+
+
+example : ∃! r : ℤ, 0 ≤ r ∧ r < 5 ∧ 14 ≡ r [ZMOD 5] := by
+  use 4
+  dsimp
+  constructor
+  · constructor
+    · numbers
+    constructor
+    · numbers
+    use 2
+    numbers
+  intro r hr
+  obtain ⟨hr1, hr2, q, hr3⟩ := hr
+  have :=
+    calc
+      5 * 1 < 14 - r := by addarith [hr2]
+      _ = 5 * q := by rw [hr3]
+  cancel 5 at this
+  have :=
+    calc
+      5 * q = 14 - r := by rw [hr3]
+      _ < 5 * 3 := by addarith [hr1]
+  cancel 5 at this
+  interval_cases q
+  addarith [hr3]
+
+/-! # Exercises -/
+
+
+example : ∃! x : ℚ, 4 * x - 3 = 9 := by
+  use 3
+  dsimp
+  constructor
+  numbers
+  intro y hy
+  calc
+    y = ((4*y-3) + 3)/4 := by ring
+    _ = (9+3)/4 := by rw[hy]
+    _ = 12/4 := by numbers
+    _ = 3 := by numbers
+
+example : ∃! n : ℕ, ∀ a, n ≤ a := by
+  use 0
+  dsimp
+  constructor
+  exact fun a ↦ Nat.zero_le a
+  intro a h
+  exact Nat.le_zero.mp (h 0)
+
+
+example : ∃! r : ℤ, 0 ≤ r ∧ r < 3 ∧ 11 ≡ r [ZMOD 3] := by
+  use 2
+  dsimp
+  constructor
+  constructor
+  numbers
+  constructor
+  numbers
+  dsimp[Int.ModEq] at *
+  dsimp[(· ∣ ·)]
+  use 3
+  numbers
+  intro a ha
+  obtain ⟨ hb, hc, hd⟩ := ha
+  interval_cases a
+  exfalso
+  dsimp[Int.ModEq] at *
+  dsimp[(· ∣ ·)] at *
+  obtain ⟨ e, he⟩ := hd
+  have h : 11 - 0 ≠ 3*e := by sorry
+  exact h he
+
+  exfalso
+  dsimp[Int.ModEq] at *
+  dsimp[(· ∣ ·)] at *
+  obtain ⟨ e, he⟩ := hd
+  have h : 11 - 1 ≠ 3*e := by sorry
+  exact h he
+
+  rfl
